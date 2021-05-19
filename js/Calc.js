dbb7afb94ed2fc6102ad0cc2936a501fe0306f16
EdBozhko <eduard.bozhko92@gmail.com>
2021-05-19 12:59:28 +0300

    added hide and close button and their functions

--- js/Calc.js
+++ js/Calc.js
@@ -8,17 +8,18 @@ class Calc {
   #processor;
   #container;
   #calc;
+  #calcContainer
   constructor() {
     this.onDigitalButtonPress = this.onDigitalButtonPress.bind(this); //для варианта с простой функцией, привязываем к calc(без этого функция кнопки передает в this кнопку, а не ссылку на сам калькулятор)
     // инициализируем создание доски с кнопками, процессора и дисплея
-    this.#header = new Header(this)
+    this.#header = new Header(this, this.onHideButtonPress.bind(this), this.onCloseButtonPress.bind(this))
     this.#board = new Board(
       this,
       this.onDigitalButtonPress,
       this.onOperationButtonPress,
       this.onEqualityButtonPress,
-      this.onNegativeButtonPress,
-      this.onDotButtonPress,
+      // this.onNegativeButtonPress,
+      // this.onDotButtonPress,
       this.onBackspaceButtonPress
     ); // передаем функции отслеживания клика из самого калькулятора в аргументы конструктора class Board
     this.#processor = new Processors(this, this.onResult, this.onMemoValue);
@@ -28,6 +29,13 @@ class Calc {
   // -----------------------------
   // функции отслеважиния клика на кнопку в Calc
 
+  onHideButtonPress(button) {
+    this.#header.hideButton.hide(this.#display.display)
+    this.#header.hideButton.hide(this.#board.board)
+  }
+  onCloseButtonPress(button) {
+    this.#header.closeButton.close(this.#calc)
+  }
   onDigitalButtonPress(button) {
     // вариант простой функции
     this.#processor.onDigitalButtonPress(button); // обращаемся к функции процессора
@@ -41,12 +49,12 @@ class Calc {
   onEqualityButtonPress = (button) => {
     this.#processor.onEqualityButtonPress(button);
   };
-  onNegativeButtonPress = (button) => {
-    this.#processor.onNegativeButtonPress(button);
-  };
-  onDotButtonPress = (button) => {
-    this.#processor.onDotButtonPress(button);
-  };
+  // onNegativeButtonPress = (button) => {
+  //   this.#processor.onNegativeButtonPress(button);
+  // };
+  // onDotButtonPress = (button) => {
+  //   this.#processor.onDotButtonPress(button);
+  // };
   onBackspaceButtonPress = (button) => {
     this.#processor.onBackspaceButtonPress(button);
   };
@@ -68,17 +76,22 @@ class Calc {
   get display() {
     return this.#display;
   }
+
   render(containerId) {
     this.#container = document.getElementById(containerId);
+this.#calcContainer = document.createElement('div')
+this.#calcContainer.setAttribute("class", "calculator-container");
+
     this.#calc = document.createElement("div");
     this.#calc.setAttribute("class", "calculator");
     this.#header.render(this.#calc)
     this.#display.render(this.#calc);
     this.#board.render(this.#calc);
-    this.#container.appendChild(this.#calc);
+    this.#calcContainer.appendChild(this.#calc)
+    this.#container.appendChild(this.#calcContainer);
     this.onResult(this.#processor.stack);
   }
 }
 
-const calc = new Calc();
-calc.render("container");
+// const calc = new Calc();
+// calc.render("container");
--- js/Header.js
+++ js/Header.js
@@ -1,18 +1,38 @@
 class Header {
   #calc;
+  #hideButton;
+  #closeButton;
+  #actionButtonsList;
   #header;
   #container;
   #calcName;
-  constructor(calc) {
+  constructor(calc, onHideButtonClick, onCloseButtonClick) {
     this.#calc = calc;
+    this.#hideButton = new HideButton(this, onHideButtonClick);
+    this.#closeButton = new CloseButton(this, onCloseButtonClick);
+  }
+  get hideButton() {
+    return this.#hideButton;
+  }
+  get closeButton() {
+    return this.#closeButton;
   }
   render(container) {
     this.#container = container;
     this.#header = document.createElement("header");
-    this.#header.setAttribute("class", "header");
+    this.#header.setAttribute("class", "header-calc");
     this.#calcName = document.createElement("span");
+    this.#calcName.setAttribute("class", "header-calc__calc-name");
     this.#calcName.innerText = "Калькулятор";
+    this.#actionButtonsList = document.createElement("div");
+    this.#actionButtonsList.setAttribute(
+      "class",
+      "header-calc__action-buttons-list"
+    );
+    this.#hideButton.render(this.#actionButtonsList);
+    this.#closeButton.render(this.#actionButtonsList);
     this.#header.appendChild(this.#calcName);
+    this.#header.appendChild(this.#actionButtonsList);
     this.#container.appendChild(this.#header);
   }
 }
